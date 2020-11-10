a8b67836ad552298911e33807e675dfac09e72cb
artandsky <krekerr85@gmail.com>
2020-11-10 17:23:47 +0300

    second commit

---
+++ artandsky/src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <button @click="handleButton">Post</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import axios from 'axios';
+export default {
+  data(){
+    return {
+      list: [],
+      errors: []
+    }
+  },
+  components: {
+    HelloWorld
+  },
+  methods:{
+    handleButton(){
+      axios.post(`/hello`)
+      .then(response => {
+      // JSON responses are automatically parsed.
+      this.list.push(response.data)
+      this.printToConsole();
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+    },
+    printToConsole(){
+      console.log(this.list);
+    }
+  }
+};
+</script>
