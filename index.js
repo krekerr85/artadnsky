dc0b5858e64a8e8ed490d6a621e1024fa4570879
artandsky <krekerr85@gmail.com>
2020-11-14 00:41:25 +0300

    hello

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+var session = require("cookie-session");
+const cors = require("cors");
+import config from "./src/config";
+import { connect } from "./src/Utils/db";
+import userRouter from "./src/resources/user/user.router";
+import { signup, signin, protect } from "./src/Utils/auth";
+const app = express();
+var expiryDate = new Date(Date.now() + 60 * 60 * 1000); // 1 hour
+app.use(
+	session({
+		name: "session",
+		keys: ["key1", "key2"],
+		cookie: {
+			secure: true,
+			httpOnly: true,
+			expires: expiryDate,
+		},
+	})
+);
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.post("/signup", signup);
+app.post("/signin", signin);
+
+app.use("/loadCurrentUser", protect);
+
+app.use("/user", userRouter);
+
+const start = async () => {
+	try {
+		await connect();
+		app.listen(config.port, () => {
+			console.log(`REST API on http://localhost:${config.port}/api`);
+		});
+	} catch (e) {
+		console.error(e);
+	}
+};
+
+start();
