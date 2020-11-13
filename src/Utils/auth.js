3ef3e06fb4815d271f7c9fbd10f0647a3b745563
artandsky <krekerr85@gmail.com>
2020-11-14 00:26:05 +0300

    pizdec

--- src/Utils/auth.js
+++ src/Utils/auth.js
@@ -1,6 +1,6 @@
-import config from "../config";
-import { User } from "../resources/user/user.model";
-import jwt from "jsonwebtoken";
+const config = require("../config");
+const { User } = require("../resources/user/user.model");
+const jwt = require("jsonwebtoken");
 
 export const newToken = (user) => {
 	return jwt.sign({ id: user.id }, config.secrets.jwt, {
